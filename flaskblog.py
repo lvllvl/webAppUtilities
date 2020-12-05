76e290c674c1c84ee4ecaa5f55f0d8cc9c373300
lvllvl <24905907+lvllvl@users.noreply.github.com>
2020-12-05 15:05:12 -0500

    update, new folders

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,14 @@
 from flask import Flask
 app = Flask( __name__ )
 
-@app.route("/") # this is a route , decorator 
+@app.route("/home") # this is a route , decorator 
 def hello(): 
-    return "Hello World!"
+    return "<h1>Hello World!</h1>"
 
+@app.route("/about") # this is a route , decorator 
+def about(): 
+    return "<h1>About</h1>"
+
+if __name__ == '__main__':
+    app.run( debug = True )
 
