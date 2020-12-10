bb7692e6b7a61469bf32ca878edf64d5d39d95c1
lvllvl <24905907+lvllvl@users.noreply.github.com>
2020-12-10 16:56:28 -0500

    New files

--- forms.py
+++ forms.py
@@ -1,31 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-# writing python classes that represent our forms 
-# then theyll be converted to HTML
 
-class RegistrationForm( FlaskForm ):
 
-    username = StringField( 'Username', 
-                    validators=[ DataRequired(), Length( min = 2, max=20) ] )
-    email = StringField( 'Email', 
-                    validators=[ DataRequired(), Email() ] )
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-    password = PasswordField( 'Passowrd', 
-                    validators=[ DataRequired(), Length( min = 8, max = 20 )  ])
 
-    confirm_password = PasswordField( 'Confirm Passowrd', 
-                    validators=[ DataRequired(), EqualTo('password'))  ] )
-
-    submit = SubmitField( 'Sign Up' ) 
-
-class LoginForms( FlaskForm ):
-
-    email = StringField( 'Email', 
-                    validators=[ DataRequired(), Email() ] )
-
-    password = PasswordField( 'Passowrd', 
-                    validators=[ DataRequired(), Length( min = 8, max = 20 )  ])
-
-    remember =  BooleanField( 'Remember Me' ) 
-    submit = SubmitField( 'Login' ) 
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
