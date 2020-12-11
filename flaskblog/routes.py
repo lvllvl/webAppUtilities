04e2ad2fa04b911eec903e101d8de7478f3d6b83
lvllvl <24905907+lvllvl@users.noreply.github.com>
2020-12-10 23:11:27 -0500

    update files

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt 
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-
+from flask_login import login_user, current_user, logout_user
 
 # TODO: delete this posts data structure 
 posts = [
@@ -20,12 +20,10 @@ posts = [
     }
 ]
 
-
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
-
 
 @app.route("/about")
 def about():
@@ -34,6 +32,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated: 
+        return redirect( url_for( 'home' )) 
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -48,11 +48,23 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated: 
+        return redirect( url_for( 'home' )) 
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+        user = User.query.filter_by( email = form.email.data ).first() 
+        if user and bcrypt.check_password_hash( user.password, form.password.data ): 
+            # if user exist and pw is valid 
+            login_user( user, remember= form.remember.data )
+            return redirect( url_for( 'home' )) 
+        else: 
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user() 
+    
+    return redirect(url_for('home'))
