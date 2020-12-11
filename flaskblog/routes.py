92a419716dd4f7269114c0713a178b9fdb373944
lvllvl <24905907+lvllvl@users.noreply.github.com>
2020-12-11 16:59:17 -0500

    new updates

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,28 +1,16 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt 
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-# TODO: delete this posts data structure 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all() 
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -66,16 +54,56 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
-@app.route("/logout")
+@app.route("/logout", methods=['GET', 'POST' ] )
 def logout():
     logout_user() 
     
     return redirect(url_for('home'))
 
+def save_picture( form_picutre ): 
+    random_hex = secrets.token_hex( 8 ) 
+    _, f_ext = os.path.splitext( form_picutre.filename ) 
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join( app.root_path, 'static/profile_pics', picture_fn )
+    output_size = ( 125, 125 ) 
+    i = Image.open( form_picutre ) 
+    i.thumbnail( output_size ) 
+    i.save( picture_path )
 
-@app.route("/account")
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'] ) 
 @login_required
 def account():
+    form = UpdateAccountForm() 
+    if form.validate_on_submit():
 
+        if form.picutre.data: 
+            picture_file = save_picture( form.picutre.data )
+            current_user.image_file = picture_file 
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit() # commit this to the database 
+        flash( 'Your account has been updated!' ,  'success' ) 
+        return redirect( url_for( 'account' )) 
+    elif request.method =='GET': 
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for( 'static', filename='profile_pics/' + current_user.image_file )
-    return render_template('account.html', title='Account', image_file= image_file )
+    return render_template('account.html', title='Account', image_file= image_file, form=form )
+
+
+@app.route("/post/new", methods= ['GET', 'POST'] )
+@login_required
+def new_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post( title = form.title.data, content = form.content.data, author = current_user ) 
+        db.session.add( post ) # add to database 
+        db.session.commit() 
+        flash( 'Your post has been created!' , 'success' ) 
+        return redirect( url_for( 'home' ) )
+    return render_template('create_post.html', title='New Post', form = form  )
